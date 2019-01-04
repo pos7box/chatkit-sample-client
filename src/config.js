12e2bd0d5c7aa49ff1b54fdd5dbb0d0e648aada7
Zan Markan <zan.markan@gmail.com>
2019-01-04 09:29:43 +0000

    Update endpoints to the nicer netlify app url

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
 const auth0Domain = 'pusher-chatkit-demo.auth0.com'
 const auth0ClientID = 'VF0kHM9szMj0aQGEfqafrdAKVwRb2a3Q'
-const auth0RedirectUri = 'https://elastic-kilby-a7cd2c.netlify.com/'
+const auth0RedirectUri = 'https://chatkit-sample.netlify.com/'
 // const auth0RedirectUri = 'http://localhost:3000' /* Testing locally */
 
-// const chatkitTokenProviderEndpoint = 'https://zmarkan-chatkit-sample-server.glitch.me/token'
-
-const chatkitTokenProviderEndpoint = 'https://elastic-kilby-a7cd2c.netlify.com/.netlify/functions/token'
+const chatkitTokenProviderEndpoint = 'https://chatkit-sample.netlify.com/.netlify/functions/token'
 
 const chatkitInstanceLocator = 'v1:us1:7954c374-f491-4c08-b71e-5abfc0a3dc89'
 
