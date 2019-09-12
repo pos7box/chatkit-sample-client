94d9f1361589695a468b6e590131ac9326826eef
pos7box <40025421+pos7box@users.noreply.github.com>
2019-09-12 16:00:15 +1000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-const auth0Domain = 'pusher-chatkit-demo.auth0.com'
-const auth0ClientID = 'VF0kHM9szMj0aQGEfqafrdAKVwRb2a3Q'
-const auth0RedirectUri = 'https://chatkit-sample.netlify.com/'
+const auth0Domain = 'dev-chatkit.au.auth0.com'
+const auth0ClientID = 'Dg0YD94HtidaSwY9duDLXLvLHBXkwmlF'
+const auth0RedirectUri = 'https://jovial-bhabha-bb6944.netlify.com/'
 // const auth0RedirectUri = 'http://localhost:3000' /* Testing locally */
 
 const chatkitTokenProviderEndpoint = '.netlify/functions/token'
 
-const chatkitInstanceLocator = 'v1:us1:7954c374-f491-4c08-b71e-5abfc0a3dc89'
+const chatkitInstanceLocator = 'v1:us1:ede2c115-4c01-4fb9-8d23-3ce3e72c87fe'
 
 export { auth0Domain, auth0ClientID, auth0RedirectUri, chatkitTokenProviderEndpoint, chatkitInstanceLocator }
